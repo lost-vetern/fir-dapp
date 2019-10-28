7020674eec6d2488c60893850e2191eab14bba05
shahzad <shazad.02@gmail.com>
2019-10-28 13:15:25 +0500

    set

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var express = require('express'),
     axios = require('axios'),
     localStrategy = require('passport-local');
 
-var app=express();
+var app = express();
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(session({secret : 'topsecreat' , resave : false, saveUninitialized : false}));
+app.use(session({ secret: 'topsecreat', resave: false, saveUninitialized: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -24,48 +24,51 @@ passport.deserializeUser(user.deserializeUser());
 
 mongoose.connect('mongodb://localhost:27017/fir');
 
+app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
-
-app.get('/fail',(req,res)=>{
-    res.json({msg:':('});
 });
 
-app.get('/signup',(req,res)=>{
+app.get('/fail', (req, res) => {
+    res.json({ msg: ':(' });
+});
+
+app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.post('/signup',(req,res)=>{
+app.post('/signup', (req, res) => {
     user.register(new user({
         username: req.body.username
-    }),req.body.password,function(err,user){
-        if(err)res.json({msg:err});
+    }), req.body.password, function(err, user) {
+        if (err) res.json({ msg: err });
         res.render('signin');
     });
 });
 
-app.get('/signin',(req,res)=>{
+app.get('/signin', (req, res) => {
     res.render('signin');
 });
 
-app.post('/signin',passport.authenticate('local'),(req,res)=>{
+app.post('/signin', passport.authenticate('local'), (req, res) => {
     console.log('i am here');
     res.render('addfir');
 });
 
 
-function auth(req,res,next){
-    if(req.isAuthenticated()){next();}
-    else res.redirect('/fail');
+function auth(req, res, next) {
+    if (req.isAuthenticated()) { next(); } else res.redirect('/fail');
 }
 
 
 ////chain routes
-require('./chainRoutes')(app,auth);
+require('./chainRoutes')(app, auth);
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log('Server is up!');
 });
--- chainRoutes.js
+++ chainRoutes.js
@@ -1,86 +1,90 @@
 var block = require('./schema');
 var sha256 = require('js-sha256');
 
-module.exports = function(app,auth){
+module.exports = function(app, auth) {
     //get list
-    app.get('/firlist',(req,res)=>{
-        block.find({},function(err,dbres){
-            if(err)console.log(err);
-            else{
-                console.log(dbres);
-                res.json({'FIRs':dbres});    
+    app.get('/firlist', (req, res) => {
+        block.find({}, function(err, dbres) {
+            if (err) console.log(err);
+            else {
+                res.render('showfir', { dbres })
+                    // console.log(dbres);
+                    // res.json({ 'FIRs': dbres });
             }
         });
     });
 
     //fir by firnumber
-    app.post('/firlistbynumber',(req,res)=>{
-        block.find({firNumber:req.body.firNumber},function(err,dbres){
-            if(err)console.log(err);
-            else{
+    app.post('/firlistbynumber', (req, res) => {
+        block.find({ firNumber: req.body.firNumber }, function(err, dbres) {
+            if (err) console.log(err);
+            else {
                 console.log(dbres);
-                res.json({FIR:dbres});    
+                res.json({ FIR: dbres });
             }
         });
     });
 
-        //fir by caseTitle
-        app.post('/firlistbycasetitle',(req,res)=>{
-            block.find({caseTitle:req.body.caseTitle},function(err,dbres){
-                if(err)console.log(err);
-                else{
-                    console.log(dbres);
-                    res.json({FIR:dbres});    
-                }
-            });
+    //fir by caseTitle
+    app.post('/firlistbycasetitle', (req, res) => {
+        block.find({ caseTitle: req.body.caseTitle }, function(err, dbres) {
+            if (err) console.log(err);
+            else {
+                console.log(dbres);
+                res.json({ FIR: dbres });
+            }
         });
+    });
 
-    app.get('/addfir',auth,(req,res)=>{
+    app.get('/addfir', auth, (req, res) => {
         res.render('addfir');
     });
-    app.post('/addFIR',auth,(req,res)=>{
-        block.find({},function(err,dbres){
-            if(err)console.log(err);
-            else{
-                var previousHashTemp,firNumberTemp;
-               if(dbres.length===0){previousHashTemp='genesis';firNumberTemp=0;}
-               else{
-                   previousHashTemp=dbres[dbres.length-1].hash;
-                   firNumberTemp=(dbres[dbres.length-1].firNumber);
-                   firNumberTemp++;
-                   console.log(firNumberTemp);}
-                   var timeStampTemp=new Date().toGMTString();
-                   var hashTemp=sha256(previousHashTemp+
-                    req.body.caseTitle+
-                    firNumberTemp+
-                    req.body.officerId+
-                    timeStampTemp+
-                    req.body.location+
-                    req.body.complaint+
-                    req.body.victims+
-                    req.body.accused+
-                    req.body.witness+
+    app.post('/addFIR', auth, (req, res) => {
+        block.find({}, function(err, dbres) {
+            if (err) console.log(err);
+            else {
+                var previousHashTemp, firNumberTemp;
+                if (dbres.length === 0) {
+                    previousHashTemp = 'genesis';
+                    firNumberTemp = 0;
+                } else {
+                    previousHashTemp = dbres[dbres.length - 1].hash;
+                    firNumberTemp = (dbres[dbres.length - 1].firNumber);
+                    firNumberTemp++;
+                    console.log(firNumberTemp);
+                }
+                var timeStampTemp = new Date().toGMTString();
+                var hashTemp = sha256(previousHashTemp +
+                    req.body.caseTitle +
+                    firNumberTemp +
+                    req.body.officerId +
+                    timeStampTemp +
+                    req.body.location +
+                    req.body.complaint +
+                    req.body.victims +
+                    req.body.accused +
+                    req.body.witness +
                     req.body.offenseSections);
-                   console.log(hashTemp);
+                console.log(hashTemp);
                 //create new chain
                 block.create(new block({
                     previousHash: previousHashTemp,
-                    hash:hashTemp,
-                    caseTitle:req.body.caseTitle,
-                    firNumber:firNumberTemp,
-                    timestamp:timeStampTemp,
-                    location:req.body.location,
-                    complaint:req.body.complaint,
-                    victims:req.body.victims,
-                    accused:req.body.accused,
-                    witness:req.body.witness,
-                    offenseSections : req.body.offenseSections 
-                }),function(err,dbres){
+                    hash: hashTemp,
+                    caseTitle: req.body.caseTitle,
+                    firNumber: firNumberTemp,
+                    timestamp: timeStampTemp,
+                    location: req.body.location,
+                    complaint: req.body.complaint,
+                    victims: req.body.victims,
+                    accused: req.body.accused,
+                    witness: req.body.witness,
+                    offenseSections: req.body.offenseSections
+                }), function(err, dbres) {
                     //console.log(err);
                     console.log(dbres);
-                    res.json({msg:dbres});
-                });        
-                     
+                    res.json({ msg: dbres });
+                });
+
             }
         });
     });
